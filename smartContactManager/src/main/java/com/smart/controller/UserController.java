7438288f087b851b5e219da8e45400f9f12c1a50
malikrajat <er.rajatmalik@gmil.com>
2021-10-16 13:30:47 +0530

    setting

--- smartContactManager/src/main/java/com/smart/controller/UserController.java
+++ smartContactManager/src/main/java/com/smart/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,9 +39,12 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepo;
-	
+
 	@Autowired
 	private ContactRepositiry contactRepositiry;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@ModelAttribute
 	public void addCommanData(Model model, Principal principal) {
@@ -63,82 +67,77 @@ public class UserController {
 	}
 
 	@PostMapping("/saveContact")
-	public String saveContact(
-			@ModelAttribute Contact contact, 
-			@RequestParam("image1") MultipartFile file, 
-			Model model, 
-			Principal principal,
-			HttpSession session
-			) {
+	public String saveContact(@ModelAttribute Contact contact, @RequestParam("image1") MultipartFile file, Model model,
+			Principal principal, HttpSession session) {
 		try {
 			model.addAttribute("title", "Add Contact");
 			String name = principal.getName();
 			User user = userRepo.getUserByUserName(name);
 			contact.setUser(user);
-			if(file.isEmpty()) {
+			if (file.isEmpty()) {
 				contact.setImage("contact.png");
 			} else {
 				contact.setImage(file.getOriginalFilename());
-				File saveFile =new ClassPathResource("static/img").getFile();
+				File saveFile = new ClassPathResource("static/img").getFile();
 				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
 				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
 				System.out.println("Image upload success");
-				session.setAttribute("message", new Message("Saved Success",  "success"));
+				session.setAttribute("message", new Message("Saved Success", "success"));
 			}
 			user.getContacts().add(contact);
 			userRepo.save(user);
 		} catch (Exception e) {
 			System.out.println("ERROR : " + e.getMessage());
-			session.setAttribute("message", new Message("Error",  "danger"));
+			session.setAttribute("message", new Message("Error", "danger"));
 			e.printStackTrace();
 		}
 		return "addContact";
 	}
-	
+
 	@GetMapping("/list/{page}")
 	public String showContact(@PathVariable("page") int page, Model model, HttpSession session, Principal principal) {
 		model.addAttribute("title", "Show Contact");
 		String nameName = principal.getName();
 		User user = userRepo.getUserByUserName(nameName);
 		Pageable pageRequest = PageRequest.of(page, 5);
-		
+
 		Page<Contact> contacts = contactRepositiry.findContactByUser(user.getId(), pageRequest);
 		model.addAttribute("contacts", contacts);
 		model.addAttribute("currentPage", page);
 		model.addAttribute("totalPages", contacts.getTotalPages());
-		
+
 		return "showContact";
 	}
-	
+
 	@GetMapping("/{id}/contact")
-	public String detail (Model m, @PathVariable("id") int id, Principal principal) {
-		
+	public String detail(Model m, @PathVariable("id") int id, Principal principal) {
+
 		String nameName = principal.getName();
 		User user = userRepo.getUserByUserName(nameName);
-		
+
 		Optional<Contact> contact = contactRepositiry.findById(id);
 		Contact contact2 = contact.get();
-		
-		if(contact2.getUser().getId() == user.getId()) {
+
+		if (contact2.getUser().getId() == user.getId()) {
 			m.addAttribute("contact", contact2);
 			m.addAttribute("title", contact2.getName());
 		}
 		return "details";
 	}
-	
+
 	@GetMapping("/delete/{id}")
-	public String delete(@PathVariable("id") int id, Model m,Principal principal, HttpSession session) throws IOException {
+	public String delete(@PathVariable("id") int id, Model m, Principal principal, HttpSession session)
+			throws IOException {
 
 		Optional<Contact> contact = contactRepositiry.findById(id);
 		Contact contact2 = contact.get();
-		
+
 		String nameName = principal.getName();
 		User user = userRepo.getUserByUserName(nameName);
-		
-		
-		if(contact2.getUser().getId() == user.getId()) {
-			//remove img
-			File saveFile =new ClassPathResource("static/img").getFile();
+
+		if (contact2.getUser().getId() == user.getId()) {
+			// remove img
+			File saveFile = new ClassPathResource("static/img").getFile();
 			Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + contact2.getImage());
 			Files.delete(path);
 			// u need unlink user from this contact because we have cascade enabled in model
@@ -148,39 +147,34 @@ public class UserController {
 			userRepo.save(user);
 
 			session.setAttribute("message", new Message("contact delete", "success"));
-			
+
 		}
-		
-		
+
 		return "redirect:/user/list/0";
 	}
 
 	@PostMapping("/edit/{id}")
-	public String edit(@PathVariable("id") int id, Model m,Principal principal, HttpSession session) {
+	public String edit(@PathVariable("id") int id, Model m, Principal principal, HttpSession session) {
 		m.addAttribute("title", "Update");
 		Contact contact = contactRepositiry.findById(id).get();
-		m.addAttribute("contact",contact);
+		m.addAttribute("contact", contact);
 		return "edit";
 	}
-	
+
 	@PostMapping("/save")
-	public String updateContact(
-			@ModelAttribute Contact contact, 
-			Model model,
-			Principal principal, 
-			@RequestParam("image1") MultipartFile file, 
-			HttpSession session ) {
+	public String updateContact(@ModelAttribute Contact contact, Model model, Principal principal,
+			@RequestParam("image1") MultipartFile file, HttpSession session) {
 
 		try {
 			model.addAttribute("title", "Update");
 			String name = principal.getName();
 			User user = userRepo.getUserByUserName(name);
 			Contact contact2 = contactRepositiry.findById(contact.getId()).get();
-			if(file.isEmpty()) {
+			if (file.isEmpty()) {
 				contact.setImage(contact2.getImage());
 			} else {
 				contact.setImage(file.getOriginalFilename());
-				File saveFile =new ClassPathResource("static/img").getFile();
+				File saveFile = new ClassPathResource("static/img").getFile();
 				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
 				Path oldImagePath = Paths.get(saveFile.getAbsolutePath() + File.separator + contact2.getImage());
 				Files.delete(oldImagePath);
@@ -189,19 +183,52 @@ public class UserController {
 			}
 			contact.setUser(user);
 			contactRepositiry.save(contact);
-			session.setAttribute("message", new Message("Contact updated",  "success"));
+			session.setAttribute("message", new Message("Contact updated", "success"));
 		} catch (Exception e) {
 			System.out.println("ERROR : " + e.getMessage());
-			session.setAttribute("message", new Message("Error",  "danger"));
+			session.setAttribute("message", new Message("Error", "danger"));
 			e.printStackTrace();
 		}
 		return "redirect:/user/" + contact.getId() + "/contact";
 	}
-	
-	@GetMapping("/profile")	
-	public String profile(Model m,Principal principa) {
+
+	@GetMapping("/profile")
+	public String profile(Model m, Principal principa) {
 		m.addAttribute("title", "Profile");
 		return "profile";
 	}
-	
+
+	@GetMapping("/setting")
+	public String setting(Model m, Principal principa) {
+		m.addAttribute("title", "Setting");
+		return "setting";
+	}
+
+	@PostMapping("/saveSetting")
+	public String saveSetting(
+			@RequestParam("oldpassword") String oldpassword,
+			@RequestParam("password") String password,
+			@RequestParam("cpassword") String cpassword, 
+			Model m,
+			Principal principa, 
+			HttpSession session) {
+		User user = userRepo.getUserByUserName(principa.getName());
+		String password2 = user.getPassword();
+		if (password == cpassword) {
+			if (this.bCryptPasswordEncoder.matches(oldpassword, password2)) {
+				user.setPassword(this.bCryptPasswordEncoder.encode(password));
+				session.setAttribute("message", new Message("Password changed !!", "success"));
+			} else {
+				System.out.println("please enter correct old password");
+				session.setAttribute("message", new Message("Please enter correct old password ", "danger"));
+				return "redirect:/user/setting";
+			}
+		} else {
+			session.setAttribute("message", new Message("Password do not match with confirm password", "danger"));
+			return "redirect:/user/setting";
+		}
+
+		return "redirect:/user/index";
+	}
+
 }
